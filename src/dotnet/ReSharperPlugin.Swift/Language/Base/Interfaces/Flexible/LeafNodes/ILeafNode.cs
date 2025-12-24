9f6d9b8d4da45e0e9a9e9ada7ca4aed7c96bd031
jaschamerle <jascha.merle@gmail.com>
2025-12-24 06:53:34 +0000

    feat(src/dotnet/ast/core): make the cloning methods of INode more specific in ILeafNode with extensive accompanying documentation. Made import adjustements to enable this change.

--- src/dotnet/ReSharperPlugin.Swift/Language/Base/Interfaces/Flexible/LeafNodes/ILeafNode.cs
+++ src/dotnet/ReSharperPlugin.Swift/Language/Base/Interfaces/Flexible/LeafNodes/ILeafNode.cs
@@ -1,8 +1,10 @@
 
+using System;
+using JetBrains.ReSharper.Psi.Tree;
 using ReSharperPlugin.Swift.Language.Base.Interfaces.Constrained.Root;
 using ReSharperPlugin.Swift.Language.Base.Interfaces.Constrained.InternalNodes;
 using ReSharperPlugin.Swift.Language.Base.Interfaces.Constrained.LeafNodes;
-
+using ReSharperPlugin.Swift.Language.Base.Interfaces.Flexible.InternalNodes;
 using ReSharperPlugin.Swift.Language.Base.Interfaces.Flexible.Root;
 
 namespace ReSharperPlugin.Swift.Language.Base.Interfaces.Flexible.LeafNodes;
@@ -30,4 +32,82 @@ namespace ReSharperPlugin.Swift.Language.Base.Interfaces.Flexible.LeafNodes;
 ///         an <see cref="ILeafNode"/> would want to do, an <see cref="INode"/> would as well.
 ///     </para>
 /// </remarks>
-public interface ILeafNode : INode;
+public interface ILeafNode : INode
+{
+    /// <summary>
+    ///     <para>
+    ///         Clone this <see cref="ILeafNode"/> as a new root <see cref="ILeafNode"/> of its own tree.
+    ///     </para>
+    ///     <para>
+    ///         The cloned <see cref="ILeafNode"/> will not be physical
+    ///         (it won't be associated to actual code in an actual file).
+    ///         The physicality of a node is returned by <see cref="ITreeNode.IsPhysical"/>.
+    ///     </para>
+    ///     <para>
+    ///         The original <see cref="ILeafNode"/> (this <see cref="ILeafNode"/>)
+    ///         will not be touched in any way and will remain physical if it is physical
+    ///         and not physical if it is not physical (the value returned from <see cref="ITreeNode.IsPhysical"/>).
+    ///         It will remain attached to its current parent (obtained from <see cref="INode.GetParent"/>).
+    ///     </para>
+    /// </summary>
+    /// <remarks>
+    ///     A cloned version of an <see cref="INode"/> is not merely known to be an <see cref="INode"/>.
+    ///     It is also an <see cref="ILeafNode"/> so this override of <see cref="INode.CloneAsDetached"/> in this interface
+    ///     merely updates this documentation and the returned type to be an <see cref="ILeafNode"/>
+    ///     as that is known to be true.
+    /// </remarks>
+    /// <returns>
+    ///     The cloned <see cref="ILeafNode"/>.
+    /// </returns>
+    public new ILeafNode CloneAsDetached();
+
+    /// <summary>
+    ///     <para>
+    ///         Clone this <see cref="ILeafNode"/> as a child of the <paramref name="newParent"/> at index <paramref name="index"/>.
+    ///         The content will be the same.
+    ///     </para>
+    ///     <para>
+    ///         An <paramref name="index"/> equal to the number of children is allowed and will result
+    ///         in simply appending the cloned <see cref="ILeafNode"/> to the end of the children of <paramref name="newParent"/>.
+    ///     </para>
+    ///     <para>
+    ///         When adding the cloned <see cref="ILeafNode"/> to the <paramref name="newParent"/> at <paramref name="index"/>,
+    ///         no child <see cref="INode"/> in <paramref name="newParent"/> will be removed. It acts the same as inserting the <see cref="ILeafNode"/>
+    ///         at <paramref name="index"/>. The element already at the <paramref name="index"/> and the later elements
+    ///         are simply moved over by one in their <see cref="INode.ParentIndex"/>es. Their <see cref="INode.ParentTextIndex"/>
+    ///         is moved over by the length of the code in the cloned <see cref="INode"/>.
+    ///     </para>
+    ///     <para>
+    ///         The cloned <see cref="ILeafNode"/> will have the physicality if the <paramref name="newParent"/>.
+    ///         The physicality of a node is returned by <see cref="ITreeNode.IsPhysical"/>.
+    ///     </para>
+    ///     <para>
+    ///         The original <see cref="ILeafNode"/> (this <see cref="ILeafNode"/>)
+    ///         will not be touched in any way and will remain physical if it is physical
+    ///         and not physical if it is not physical (the value returned from <see cref="ITreeNode.IsPhysical"/>).
+    ///         It will remain attached to its current parent (obtained from <see cref="INode.GetParent"/>).
+    ///     </para>
+    /// </summary>
+    /// <param name="newParent">
+    ///     The parent to which to add the cloned <see cref="ILeafNode"/>.
+    /// </param>
+    /// <param name="index">
+    ///     The index of the cloned <see cref="ILeafNode"/> in the <paramref name="newParent"/>.
+    /// </param>
+    /// <returns>
+    ///     The cloned <see cref="ILeafNode"/>.
+    /// </returns>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     If the <paramref name="index"/> is negative (less than 0).
+    /// </exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     If the <paramref name="index"/> is greater or equal to the number of children (returned by <see cref="IInternalNode.NumberOfChildren"/>).
+    /// </exception>
+    /// <remarks>
+    ///     A cloned version of an <see cref="INode"/> is not merely known to be an <see cref="INode"/>.
+    ///     It is also an <see cref="ILeafNode"/> so this override of <see cref="INode.CloneAsAttachedTo"/> in this interface
+    ///     merely updates this documentation and the returned type to be an <see cref="ILeafNode"/>
+    ///     as that is known to be true.
+    /// </remarks>
+    public new ILeafNode CloneAsAttachedTo(IInternalNode newParent, int index);
+}
